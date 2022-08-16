52933597ae61d120c45cc443334ed20d63bd140d
ChandradipMoonlight <chandradipshivankar@gmail.com>
2022-08-17 00:23:37 +0530

    Validated data

--- src/main/java/com/blog/app/dto/UserDTO.java
+++ src/main/java/com/blog/app/dto/UserDTO.java
@@ -1,5 +1,11 @@
 package com.blog.app.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +17,18 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
 	
 	private Integer userId;
+	
+	@NotEmpty(message = "UserName Can Not Be Empty!")
+	@Size(min = 4, message = "UserName Must be min of 4 Charactor")
 	private String userName;
+	
+	@Email(message = "Email Adressw Is Not Valid!")
 	private String email;
+
+	@NotEmpty(message = "Password Can Not Be Empty!")
+	@Size(min=3, max = 8, message = "Password must be min of 3 chars and max of 8 chars!")
 	private String password;
+	
+	@NotEmpty(message = "About Can Not Be Empty!")
 	private String about;
 }
